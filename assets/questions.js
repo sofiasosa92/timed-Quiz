240edd1a6c5958392adf0919d49355cc4fcc6ba3
Sofia92 <sofiasosaperez@outlook.com>
2022-11-13 16:40:42 -0600

    added question prompts, will add actual question towards the end of the project

---
+++ assets/questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    //this will be my questions that will be displayed in the quiz
+    {
+        title: "bla bla bla ",
+        choices: ["5", "54", "3", "6"],
+        answer: "5"
+    },
+    {
+        title: "me me me me  ",
+        choices: ["5", "54", "3", "6"],
+        answer: "54"
+    },
+    {
+        title: "na na na  ",
+        choices: ["5", "54", "3", "6"],
+        answer: "3"
+    },
+    {
+        title: "he he he he ",
+        choices: ["5", "54", "3", "6"],
+        answer: "6"
+    },
+];
