961c7ee675a16ea000dd09f7a7066a8fbbcf82b1
Sofia92 <sofiasosaperez@outlook.com>
2022-11-13 18:13:57 -0600

    added array of questions and answers

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var questions = [
     //this will be my questions that will be displayed in the quiz
     {
-        title: "bla bla bla ",
-        choices: ["5", "54", "3", "6"],
+        prompt: "bla bla bla ",
+        options: ["5", "54", "3", "6"],
         answer: "5"
     },
     {
@@ -21,3 +21,49 @@ var questions = [
         answer: "6"
     },
 ];
+
+//Dom elements
+
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#timer");
+var choicesEl = document.querySelector("#choices"); // Review this one
+var submitButton = document.querySelector("#submit");
+var startButton = document.querySelector("#start");
+var nameEl = document.querySelector("#name");
+var feedbackEl = document.querySelector("#feedback");
+var playAgainButton = document.querySelector("#playagain");
+
+//Start of quiz
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+//Begin quiz, new page
+
+function quizStart() {
+    timerId = setInterval(clock, 1000);
+    timerEl.textContent = time;
+    var landingScreenEl = document.getElementById("start-screen");
+    landingScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    getQuestion();
+}
+
+//Array of questions and answers
+
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var promptEl = document.getElementById("question-words")
+    promptEl.textContent = currentQuestion.prompt;
+    choicesEl.innerHTML = "";
+    currentQuestion.options.forEach(function(choice, i) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.textContent = i + 1 + ". " + choice;
+        choiceBtn.onclick = questionClick;
+        choicesEl.appendChild (choiceBtn);
+    })
+
+}
+
