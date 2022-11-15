a21a15f1b83c1ce3c6731057dcfd21011ef0db59
Sofia92 <sofiasosaperez@outlook.com>
2022-11-15 13:49:31 -0600

    added items to files

---
+++ assets/highscores.js
@@ -0,0 +1,24 @@
+var scoresButton = document.querySelector("#see-high-scores");
+
+function showHighscores() {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+    highscores.forEach(function(score) {
+        var liTag = document.createElement("li");
+        liTag.textContent = score.name + " - " + score.score;
+        var olEl = document.getElementById("highscores");
+        olEl.appendChild(liTag);
+    });
+}
+
+//clear scores
+
+function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+} document.getElementById("clear").onclick = clearHighscores;
+
+showHighscores();
+
--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,22 @@
 var questions = [
     //this will be my questions that will be displayed in the quiz
     {
-        prompt: "bla bla bla ",
-        options: ["5", "54", "3", "6"],
-        answer: "5"
+        prompt: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<javascript>", "<js>", "<script>", "<scripting>"],
+        answer: "<script>"
     },
     {
-        title: "me me me me  ",
+        prompt: "me me me me  ",
         choices: ["5", "54", "3", "6"],
         answer: "54"
     },
     {
-        title: "na na na  ",
+        prompt: "na na na  ",
         choices: ["5", "54", "3", "6"],
         answer: "3"
     },
     {
-        title: "he he he he ",
+        prompt: "he he he he ",
         choices: ["5", "54", "3", "6"],
         answer: "6"
     },
@@ -27,7 +27,7 @@ var questions = [
 var questionsEl = document.querySelector("#questions");
 var timerEl = document.querySelector("#timer");
 var choicesEl = document.querySelector("#choices"); // Review this one
-var submitButton = document.querySelector("#submit");
+var submitButton = document.querySelector("#submit-score");
 var startButton = document.querySelector("#start");
 var nameEl = document.querySelector("#name");
 var feedbackEl = document.querySelector("#feedback");
@@ -57,7 +57,7 @@ function getQuestion() {
     var promptEl = document.getElementById("question-words")
     promptEl.textContent = currentQuestion.prompt;
     choicesEl.innerHTML = "";
-    currentQuestion.options.forEach(function(choice, i) {
+    currentQuestion.choices.forEach(function(choice, i) {
         var choiceBtn = document.createElement("button");
         choiceBtn.setAttribute("value", choice);
         choiceBtn.textContent = i + 1 + ". " + choice;
@@ -67,3 +67,87 @@ function getQuestion() {
 
 }
 
+//In this section we are deducting time for wrong answers
+
+function questionClick() {
+    if(this.value !== questions[currentQuestionIndex].answer) {
+        time -= 10;
+        if(time < 0) {
+            time = 0;
+        }
+
+        timerEl.textContent = time;
+        feedbackEl.textContent = 'Wrong! The correct answer was ${questions[currentQuestionIndex].answer}.';
+        feedbackEl.styleColor = "red";
+    } else {
+        feedbackEl.textContent = "Correct!";
+        feedbackEl.styleColor = "green";
+    }
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 2000);
+    currentQuestionIndex++;
+    if(currentQuestionIndex === undefined) {
+        quizEnd();
+    } else {
+        getQuestion();
+    }
+}
+
+//End of the quiz
+
+function quizEnd() {
+    clearInterval(timerId);
+    var endScreen = document.getElementById("quiz-end");
+    endScreen.removeAttribute("class");
+    endScreen.innerHTML = "hello"
+    var finalScore = document.getElementById("score-final");
+    finalScore.textContent = time;
+    questionsEl.setAttribute("class", "hide");
+}
+
+//When timer reacher to 0
+
+function clock() {
+    time--;
+    timerEl.textContent = time;
+    if(time <= 0) {
+        quizEnd();
+    }
+}
+
+//Log player score and initials
+
+function saveHighscore() {
+    var name = nameEl.value.trim();
+    if(name !== "") {
+        var highscores = 
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+        var newScore = {
+            score: time,
+            name: name
+        };
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    }
+}
+
+//Save players score
+
+function checkForEnter(event) {
+    if(event.key === "Enter") {
+        saveHighscore();
+    }
+}
+nameEl.onKeyup = checkForEnter;
+
+//Save score
+// submitButton.onclick = saveHighscore;
+
+submitButton.addEventListener('submit', saveHighscore)
+
+//start quiz
+
+
+startButton.addEventListener("click", quizStart)
